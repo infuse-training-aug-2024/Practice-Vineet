f53ebb14ff65087c05c90430c854d7bb64f7a149
Vineet-Sawant <vineetswant@gmail.com>
2024-08-22 14:34:28 +0530

    created calculator.cpp file and wrote calculator program

---
+++ calculator.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator {
+public:
+    // Method to perform addition
+    double add(double a, double b) {
+        return a + b;
+    }
+    
+    // Method to perform subtraction
+    double subtract(double a, double b) {
+        return a - b;
+    }
+    
+    // Method to perform multiplication
+    double multiply(double a, double b) {
+        return a * b;
+    }
+    
+    // Method to perform division
+    double divide(double a, double b) {
+        if (b != 0)
+            return a / b;
+        else {
+            cout << "Error! Division by zero." << endl;
+            return 0;
+        }
+    }
+};
+
+int main() {
+    Calculator calc;
+    double num1, num2;
+    char operation;
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+    
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+    
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+    double result;
+    switch(operation) {
+        case '+':
+            result = calc.add(num1, num2);
+            break;
+        case '-':
+            result = calc.subtract(num1, num2);
+            break;
+        case '*':
+            result = calc.multiply(num1, num2);
+            break;
+        case '/':
+            result = calc.divide(num1, num2);
+            break;
+        default:
+            cout << "Error! Operator is not correct" << endl;
+            return 1;
+    }
+    
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
