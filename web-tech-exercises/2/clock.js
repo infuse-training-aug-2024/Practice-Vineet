906016376c13260f14b457dd2d954063c85594b4
Vineet-Sawant <vineetswant@gmail.com>
2024-08-30 10:18:52 +0530

    used now.toLocaleTimeString('en-US') -- resolved

--- web-tech-exercises/2/clock.js
+++ web-tech-exercises/2/clock.js
@@ -1,10 +1,7 @@
 function clock_time() {
     const clock = document.getElementById('clock');
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    clock.textContent = `${hours}:${minutes}:${seconds}`;
+    clock.textContent = now.toLocaleTimeString('en-US');
   }
   function toggleTheme() {
     const body = document.body;
@@ -18,5 +15,4 @@ function clock_time() {
     }
   }
   setInterval(clock_time, 1000);  
-  clock_time();
   document.getElementById('toggle-theme').addEventListener('click', toggleTheme);
